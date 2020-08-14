e12b1bcb51b1186c0efe99254b37bd67161adabf
Jia <jiavon@ualberta.ca>
2020-08-13 19:09:43 -0600

    main.css and script.js page are all linked

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parsley's Blog</title>
+
+    <!--Style(s)-->
+    <link rel="stylesheet" href="css/main.css">
+
+    <!--Script(s)-->
+    <script type="text/JavaScript" src="js/scripts.js" defer></script>
 </head>
+<h1>Parsley's Blog</h1>
 <body>
     
